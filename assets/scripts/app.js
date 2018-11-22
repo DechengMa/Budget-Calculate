ee9a9fa60ed5e070fdf5deff1f3d0348d04da028
Decheng <chinadecheng@gmail.com>
2018-11-22 20:56:14 +1100

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,25 @@
+var budgetController = (function() {
+    var x = 23;
+    
+    var add = function(a) {
+        return x + a;
+    }
+    
+    return {
+        publicTest: function(b) {
+           return add(b);
+        }
+    }
+})();
+
+var UIController = (function() {
+    
+})();
+
+var controller = (function(budgetContr, UIContr) {
+        
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        console.log('Clicked');
+    })
+    
+})(budgetController, UIController);
